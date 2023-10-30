8b870181e23f4dbfe095e3d08ee63f56661ef3b1
Debian Content Management System <146689648+DebianCMS@users.noreply.github.com>
2023-10-29 20:05:36 -0700

    Add files via upload

--- Software Project/CerberusCMS/Version Number — 4/Build Number — 0.8/Edit Number — 4325/Manual Edit Number — 208/Nyx/Source/Cerberus/Architect/MySQL/MySQL_Generator.php
+++ Software Project/CerberusCMS/Version Number — 4/Build Number — 0.8/Edit Number — 4325/Manual Edit Number — 208/Nyx/Source/Cerberus/Architect/MySQL/MySQL_Generator.php
@@ -519,16 +519,6 @@ article_title VARCHAR(50),
 PRIMARY KEY (id)
 );
 
-/* Commander */
-
-CREATE TABLE {$_MySQL_Generator_POST_ACCESS_DATABASE_TABLE_NAME_PREFIX}_commander (
-id MEDIUMINT NOT NULL AUTO_INCREMENT,
-command_execute VARCHAR(500),
-command_receive VARCHAR(500),
-command_send VARCHAR(500),
-PRIMARY KEY (id)
-);
-
 /* Comments */
 
 CREATE TABLE {$_MySQL_Generator_POST_ACCESS_DATABASE_TABLE_NAME_PREFIX}_comments (
@@ -902,6 +892,18 @@ rank_gender_male_4 VARCHAR(25),
 rank_gender_none VARCHAR(25),
 PRIMARY KEY (id)
 );
+
+/* Services Commander */
+
+mysql_query("CREATE TABLE \"$_ARCHITECT_INSTALL_FORM_POST_INSTALL_DATABASE_SERVER_DATABASE_TABLES_CONNECT_NAME_DATABASE_TABLE_PREFIX\"_services_commander (
+id MEDIUMINT NOT NULL AUTO_INCREMENT,
+command_execute VARCHAR(500),
+command_receive VARCHAR(500),
+command_send VARCHAR(500),
+command_timestamp VARCHAR(100),
+command_type VARCHAR(100),
+PRIMARY KEY (id)
+") or die(mysql_error());
 
 /* Settings */
 
