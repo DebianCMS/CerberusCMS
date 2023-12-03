7ffde1e5deef2a1e46493d8bd373e3f87e274246
Debian Content Management System <146689648+DebianCMS@users.noreply.github.com>
2023-12-02 18:42:56 -0800

    Add files via upload

--- Projects/Software Projects/CerberusCMS/PHP 7/PHP 7.4/MySQL - Improved/Version Number — 4/Build Number — 0.8/Edit Number — 4325/Manual Edit Number — 231/Extras/Installation_ToolKit/Version Number - 4/Build Number - 0.8/Edit Number - 20/Installation_ToolKit.php
+++ Projects/Software Projects/CerberusCMS/PHP 7/PHP 7.4/MySQL - Improved/Version Number — 4/Build Number — 0.8/Edit Number — 4325/Manual Edit Number — 231/Extras/Installation_ToolKit/Version Number - 4/Build Number - 0.8/Edit Number - 20/Installation_ToolKit.php
@@ -1,40 +1,45 @@
 <?php
 /*
  ============================================================================================================
+ + ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ + ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  + Cerberus Content Management System
- + ---
- + - Author 		     : Gary Christopher Johnson - Rosedale, California
- + - Electronic Mail Address : TinkeSoftware@Protonmail.com
- + - Company		     : Tinke Software
- + - Company Address	     : Rosedale, California, U.S.A.
- + - Document Notes	     : View this file in a non-formatting text editor without word-wrap for the correct
- +			       display of this programming code and its indentation.
- + ---
- +
- +
- +
- +
- +
- +
- +
- +
- +
- +
- +
- + ---
- + - File Location: root->Cerberus->Architect->MySQL->ToolKit->Installation_ToolKit.php
- + - File Version : 0.6 - Wednesday, March 1st of 2023
- + ---
+ + ----------------------------------------------------------------------------------------------------------
+ + - Author			 : Gary Christopher Johnson of Oildale and SouthGate In Southern California
+ + - Company			 : Tinke Software, Free and Open Source Software
+ + - Company Address		 : Oildale In Southern California, United States of America
+ + - Electronic Mail Address	 : CerberusCMS@Protonmail.com, TinkeSoftware@Protonmail.com
+ + - Document Notes		 : View this file in a non-formatting text editor without word-wrap for the
+ +				 : correct display of this programming code and its indentation.
+ + ----------------------------------------------------------------------------------------------------------
+ +    ()    ()  ()  ()
+ +  ()  ()() () () -
+ +  ------------------------------
+ +  Pre-Installation Application
+ + ----------------------------------------------------------------------------------------------------------
+ + - This File, Location	 : Root Directory => Architect => ToolKit => Installation_ToolKit.php
+ + - This File, Version		 : 0.7.1
+ + - This File, Programming Code : Pure Pre-Hyper-Text-Post-Processor
+ + - Programming Code Model	 : Procedural, Functional, Object Oriented :: Pre-Order Algebraic
+ + -				 :					   :: Pre-Order Logical
+ + - Compatibility		 : Pre-Hyper-Text-Post-Processor 	   :: Version Numbers: 5, 7, 8
+ + -				 : Zend Engine				   :: Version Numbers: 3, 4
+ + - Official Release Date	 : Wednesday, November 1st of 2023
+ + ----------------------------------------------------------------------------------------------------------
+ + -------------------------------------------------------------------------------	 - Nyx -
+ + --[][]--[][][]--[][][]--[][][]---[][][]--[][][]--[]--[]------[][][]------------   []++++||=======>
+ + -[]-----[]------[]--[]--[]---[]--[]------[]--[]--[]--[]------[]----------------
+ + -[]-----[]------[]--[]--[]---[]--[]------[]--[]--[]--[]------[]----------------    |'-._/\_.-'|
+ + -[]-----[]------[]--[]--[]---[]--[]------[]--[]--[]--[]------[]----------------    |    []    |
+ + -[]-----[]------[]--[]--[]---[]--[]------[]--[]--[]--[]------[]----------------    |___-[]-___|
+ + -[]-----[][][]--[][][]--[][][]---[][][]--[][][]--[]--[]------[]----------------    |__((**))__|
+ + -[]-----[]------[]--[]--[]---[]--[]------[]--[]--[]--[]------[]----------------    \   -[]-   /
+ + -[]-----[]------[]--[]--[]---[]--[]------[]--[]--[]--[]------[]------------/-\-     \   []   /
+ + -[]-----[]------[]--[]--[]---[]--[]------[]--[]--[]--[]------[]---VERSION--|4|-	\  []  /
+ + --[][]--[][][]--[]--[]--[][][]---[][][]--[]--[]--[][][]--[][][]------------\-/-	 '.[].'
  + -------------------------------------------------------------------------------
- + --()()--()()()--()()()--()()()---()()()--()()()--()--()------()()()------------
- + -()-----()------()--()--()---()--()------()--()--()--()------()----------------
- + -()-----()------()--()--()---()--()------()--()--()--()------()----------------
- + -()-----()------()--()--()---()--()------()--()--()--()------()----------------
- + -()-----()()()--()()()--()()()---()()()--()()()--()--()------()----------------
- + -()-----()------()--()--()---()--()------()--()--()--()------()----------------
- + -()-----()------()--()--()---()--()------()--()--()--()------()------------/-\-
- + -()-----()------()--()--()---()--()------()--()--()--()------()------------|4|-  ~ Wynn ~
- + --()()--()()()--()--()--()()()---()()()--()--()--()()()--()()()------------\-/- Build: 0.8
+ + ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Build Number: 0.8 - Final
+ + ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Edit Number : 4,325
  ============================================================================================================
 */
 
@@ -43,7 +48,7 @@
  +
  +
  +
- + [ ^ ] Cerberus Content Management System :: Installation :: Installation ToolKit
+ + [ ^ ] Extra :: Application :: Installation :: Installation ToolKit
  +
  +
  +
@@ -53,7 +58,73 @@
 /*
  ============================================================================================================
  +
- + Hyper-Text-Markup-Language :: Document :: Start
+ +
+ + [ @ ] Error Reporting System
+ +
+ +
+ ============================================================================================================
+*/
+
+/*
+ ============================================================================================================
+ +
+ + Error Reporting System :: Error Reporting: Off: All
+ +
+ ============================================================================================================
+*/
+
+error_reporting(0);
+
+/*
+ ============================================================================================================
+ +
+ + Error Reporting System :: Error Reporting: Off: Warning AND Notice
+ +
+ ============================================================================================================
+*/
+
+// [ C ] error_reporting("E_WARNING ^ E_NOTICE");
+
+/*
+ ============================================================================================================
+ +
+ + Error Reporting System :: Error Reporting: On: Error, Warning, Parse AND Notice
+ +
+ ============================================================================================================
+*/
+
+// [ C ] error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
+/*
+ ============================================================================================================
+ +
+ + Error Reporting System :: Error Reporting: On: All
+ +
+ ============================================================================================================
+*/
+
+// [ C ] error_reporting(-1);
+
+/*
+ ============================================================================================================
+ +
+ +
+ +
+ + [ @ ] Include :: Configuration Files
+ +
+ +
+ +
+ ============================================================================================================
+*/
+
+include_once ("../../System/Configuration/Global_Configuration.php");
+include_once ("../../System/Configuration/Global_Server_Configuration.php");
+include_once ("../../System/Configuration/Global_SQL_Server_Database_Class_MySQL_Standard");
+
+/*
+ ============================================================================================================
+ +
+ + [ @ ] Hyper-Text-Markup-Language :: Document :: Start
  +
  ============================================================================================================
 */
@@ -63,35 +134,51 @@ echo ("
 
 <HTML>
 	<HEAD>
-		<TITLE>Cerberus Content Management System - Post Installation ToolKit</TITLE>
+		<TITLE>$_PROJECT_STRING_NAME_EXTENDED - Pre-Installation ToolKit</TITLE>
 		<LINK REL=\"stylesheet\" HREF=\"../../Theme/Global/Style_Sheet/Style.css\" TYPE=\"text/css\">
 		<META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html;charset=utf-8\">
 		</HEAD>
 		
 		<BODY>
-				<CENTER>Cerberus Content Management System - Post-Installation ToolKit</CENTER>
-				<CENTER>[&nbsp;<A HREF=\"?\" TITLE=\"Close All Open Applications\">Close Open Applications</A>&nbsp;&middot;&nbsp;</A><A HREF=\"?Application&#61;Unpack\" TITLE=\":: Unpack Downloaded Archives of Cerberus Content Management System To This Web Server ::\">Unpack Cerberus Content Management System Compressed Package</A>&nbsp;&middot;&nbsp;<A HREF=\"?Application&#61;Terminal\" TITLE=\":: Open and Display The Post-Installation ToolKit Terminal Application ::\">Execute Terminal Commands</A>&nbsp;&middot;&nbsp;<A HREF=\"../../Architect.php\" TITLE=\":: Open & Display The Cerberus Content Management System Architect Installation Application ::\">Architect Installation Application</A>&nbsp;]</CENTER>
+				<CENTER>$_PROJECT_STRING_NAME_EXTENDED - Pre-Installation ToolKit</CENTER>
+				<CENTER>[&nbsp;<A HREF=\"?\" TITLE=\":: Close All Open Applications ::\">Close Open Applications</A>&nbsp;&middot;&nbsp;</A><A HREF=\"?Application&#61;Unpack\" TITLE=\":: Unpack Downloaded Archives of $_PROJECT_STRING_NAME_EXTENDED To This Web Server ::\">Unpack $_PROJECT_STRING_NAME_EXTENDED Compressed Archive Package</A>&nbsp;&middot;&nbsp;<A HREF=\"?Application&#61;Terminal\" TITLE=\":: Open & Display :: The Post-Installation ToolKit Terminal Application ::\">Execute Terminal Commands</A>&nbsp;&middot;&nbsp;<A HREF=\"../../Architect.php\" TITLE=\":: Open & Display The $_PROJECT_STRING_NAME_EXTENDED Architect Installation Application ::\">Architect Installation Application</A>&nbsp;]</CENTER>
 				<HR>
 ");
 
 /*
  ============================================================================================================
  +
- + Unpack Packages
+ + [ @ ] Unpack Packages
  +
  ============================================================================================================
 */
 	
 if ($_GET["Application"] == "Unpack") {
 
-system('unzip Cerberus.zip');
+$_Installation_ToolKit_POST_ZIP_ARCHIVE_UNPACK_FILE			= $_POST['Installation_ToolKit_Zip_Archive_Unpack_File'];
+$_Installation_ToolKit_POST_ZIP_ARCHIVE_UNPACK_FILE_LOCATION		= $_POST['Installation_ToolKit_Zip_Archive_Unpack_File_Location'];
+$_Installation_ToolKit_ZIP_Archive_Initialize				= new ZipArchive;
+$_Installation_ToolKit_ZIP_Archive_Unpack				= $_Installation_ToolKit_ZIP_Archive_Initialize->open('$_Installation_ToolKit_POST_ZIP_ARCHIVE_UNPACK');
 
-} // [ + ] IF: System: Unzip Cerberus Archive
+if ($_Installation_ToolKit_ZIP_Archive_Unpack === TRUE) {
+
+$_Installation_ToolKit_ZIP_Archive_Initialize->extractTo('$_Installation_ToolKit_POST_ZIP_ARCHIVE_UNPACK_FILE_LOCATION');
+$_Installation_ToolKit_ZIP_Archive_Initialize->close();
+
+echo ("ToolKit Message: The File: $_Installation_ToolKit_POST_ZIP_ARCHIVE_UNPACK_FILE Was Unpacked To Location: $_Installation_ToolKit_POST_ZIP_ARCHIVE_UNPACK_FILE_LOCATION");
+
+} else {
+
+echo ("ToolKit Message: There was an error. I was not able to unpack the archive file.");
+
+}
+
+} // [ + ] IF: System: Unzip Downloaded Archive Package
 
 /*
  ============================================================================================================
  +
- + Execute Terminal Commands
+ + [ @ ] Execute Terminal Commands
  +
  ============================================================================================================
 */
@@ -104,8 +191,8 @@ if (!$_Installation_ToolKit_POST_TERMINAL_COMMANDS) {
 
 echo ("
 			<FORM ACTION=\"?Application&#61;Terminal\" METHOD=\"POST\">
-				Please Visit: <A HREF=\"https://www.GitHub.com/TinkeSoftware/CerberusCMS_Archives/\">Tinke Softwares' - Cerberus Content Management System Archives - GitHub Repository</A> for the most recent Archived CerberusCMS compressed package<BR>
-				<TEXTAREA ROWS=\"15\" COLS=\"75\" NAME=\"Installation_ToolKit_Terminal_Commands\">Execute Terminal Commands On This Operating System From This Post-HypterText-PreProcessor Script E.G.: 'wget -O Cerberus.zip https://GitHub.com/TinkeSoftware/<File_Name_Here.zip>'</TEXTAREA><BR>
+				Please Visit: <A HREF=\"$_PROJECT_SERVER_URL_GITHUB_SECURE\">$_PROJECT_DEVELOPER_STRING_NAME_EXTENDED' - $_PROJECT_STRING_NAME_EXTENDED Archive Packages - Official GITHub Repository</A> for the most recent Archived $_PROJECT_STRING_NAME_UNIX compressed package<BR>
+				<TEXTAREA ROWS=\"15\" COLS=\"75\" NAME=\"Installation_ToolKit_Terminal_Commands\">Execute Terminal Commands On This Operating System From This Pre-Hyper-Text-Post-Processor Script E.G.: 'wget -O $_PROJECT_STRING_NAME_SHORT.zip https://GitHub.com/$_PROJECT_SERVER_URL_PARTIAL/<File_Name_Here.zip>'</TEXTAREA><BR>
 				<INPUT TYPE=\"SUBMIT\" VALUE=\"Execute Terminal Command\">
 			</FORM>
 ");
@@ -125,7 +212,7 @@ $_Installation_ToolKit_EXECUTE_TERMINAL_COMMAND				= shell_exec($_Installation_T
 /*
  ============================================================================================================
  +
- + Hyper-Text-Markup-Language :: Document :: End
+ + [ @ ] Hyper-Text-Markup-Language :: Document :: End
  +
  ============================================================================================================
 */
